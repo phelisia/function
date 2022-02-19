e2226db314834768e2a07ea12562b2483e270b9e
phelisa <phelisajeruto@gmail.com>
2022-02-19 18:40:56 +0300

    added a function

--- src/main.kt
+++ src/main.kt
@@ -1,9 +1,10 @@
 fun main (){
-    salams("Ada")
+    salams("ada")
     var x = twoNumbers(30,4)
     println(x)
     var you= intrestingMe("chips","Bible")
     println(you)
+    fourNumbers(40,20,30,50)
 }
 fun salams (name:String){
     println("hello $name")
@@ -12,6 +13,10 @@ fun salams (name:String){
 fun twoNumbers(oneNum:Int,secNum:Int):Int{
     var numbers =oneNum%secNum
     return numbers
+}
+fun fourNumbers(num1:Int,num2:Int,num3:Int,num4:Int){
+    var sum=num1+num2+num3+num4
+    println(sum)
 }
 fun intrestingMe(food:String,book: String){
 var intrest= food+book
